e4126128016f04c085aa4881219ec6b0518191d6
samsostudios <sam@samsostudios.com>
2023-01-27 21:55:42 -0700

    updates to three boilerplate

--- src/three/scene.js
+++ src/three/scene.js
@@ -1,3 +1,5 @@
+// import fragment from "./shaders/fragment.glsl";
+// import vertex from "./shaders/vertex.glsl";
 import { gsap } from "gsap";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
@@ -7,22 +9,26 @@ console.log("/// threeJS ///");
 export default class Sketch {
   constructor() {
     this.time = 0;
-    this.createRenderer();
-    this.createCamera();
-    this.createScene();
-    this.addMesh();
+    this.container = document.getElementById("threeSection");
+
+    this.init();
+    this.createControls();
+    this.addObject();
+    this.setupResize();
+    this.resize();
     this.render();
   }
 
-  createRenderer() {
+  init() {
+    // Renderer
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document
       .getElementById("threeSection")
       .appendChild(this.renderer.domElement);
-  }
 
-  createCamera() {
+    // Camera
     this.camera = new THREE.PerspectiveCamera(
       70,
       window.innerWidth / window.innerHeight,
@@ -30,24 +36,46 @@ export default class Sketch {
       10
     );
     this.camera.position.z = 1;
-  }
 
-  createScene() {
+    // Scene
     this.scene = new THREE.Scene();
   }
 
-  addMesh() {
+  createControls() {
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+  }
+
+  addObject() {
     this.geometry = new THREE.PlaneGeometry(1, 1);
     this.material = new THREE.MeshNormalMaterial({ side: THREE.DoubleSide });
+    // this.shaderMaterial = new THREE.ShaderMaterial({
+    //   fragmentShader: fragment,
+    //   vertexShader: fragment,
+    //   uniforms: {progress},
+    // });
 
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     this.scene.add(this.mesh);
   }
 
+  setupResize() {
+    window.addEventListener("resize", this.resize.bind(this));
+  }
+
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+    this.camera.updateProjectionMatrix();
+  }
+
   render() {
-    this.time += 1;
-    this.mesh.rotation.x += 0.001;
-    this.mesh.rotation.y += 0.003;
+    // this.time += 1;
+    // this.mesh.rotation.x += 0.001;
+    // this.mesh.rotation.y += 0.003;
+
+    this.controls.update();
 
     this.renderer.render(this.scene, this.camera);
     window.requestAnimationFrame(this.render.bind(this));
